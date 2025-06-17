e51849530e1b7bafa6e6dabad67ef8383e0b7a1c
Jesse <jesse.caddell@wsu.edu>
2025-06-16 18:00:01 -0700

    correct expo set up

---
+++ habit-champions-nextjs/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-white text-xl font-bold">NativeWind Works!</Text>
+    </View>
+  );
+}
+
